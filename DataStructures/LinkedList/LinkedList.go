7578eb6ad6084b98f79ade9096165471eda34972
Amit K <amit3992@gatech.edu>
2018-05-18 18:42:10 -0700

    Alright, I'm home now. Un-fucking up linkedlist DS

--- DataStructures/LinkedList/LinkedList.go
+++ DataStructures/LinkedList/LinkedList.go
@@ -1,22 +1,57 @@
 package LinkedList
 
 import (
-	"fmt"
 	"sync"
-	
+
 	"github.com/cheekybits/genny/generic"
 )
 
+/* LinkedList type is generic */
 type Item generic.Type
 
+/* Node struct */
 type Node struct {
 	content Item
-	next *Node
+	next    *Node
 }
 
+/* LinkedList struct */
 type LinkedList struct {
-	head *Node
-	size int
-	lock sync.RWMutex
+	head  *Node
+	count int
+	lock  sync.RWMutex
 }
 
+/* Function to check if linkedlist is empty */
+func (ll *LinkedList) isEmpty() bool {
+	return ll.head == nil
+}
+
+/* Function to append an element to the linkedlist */
+func (ll *LinkedList) Append(t Item) {
+	ll.lock.Lock()
+	node := Node{t, nil}
+
+	if ll.head == nil {
+		ll.head = &node
+	} else {
+		last := ll.head
+
+		for {
+			if ll.head == nil {
+				break
+			}
+			last = last.next
+		}
+
+		last.next = &node
+	}
+
+	ll.count++
+	ll.lock.Unlock()
+}
+
+/* Return linkedlist size */
+func (ll *LinkedList) size() int {
+	return ll.count
+}
